d0639d64ce610712075af57af6b3f136c6178369
DESKTOP-M3T0SOU\tharu <tharushashehan@gmail.com>
2020-03-04 14:05:55 +0530

    Initial commit

---
+++ src/main/java/com/portal/SmPortalApplication.java
@@ -0,0 +1,13 @@
+package com.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmPortalApplication.class, args);
+	}
+
+}
